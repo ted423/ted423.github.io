c7e0dbc85a484586928292fc8f36ec6444d75d74
ted423 <priston910423@gmail.com>
2024-08-02 17:33:34 +0800

    Site updated: 2024-08-02 17:33:25

--- js/pjax.js
+++ js/pjax.js
@@ -1,38 +1,51 @@
-(function() {
-    // eslint-disable-next-line no-unused-vars
-    let pjax;
+/* global NexT, CONFIG, Pjax */
 
-    function initPjax() {
-        try {
-            const Pjax = window.Pjax || function() {};
-            pjax = new Pjax({
-                selectors: [
-                    'head title',
-                    '.columns',
-                    '.navbar-start',
-                    '.navbar-end',
-                    '.searchbox',
-                    '#back-to-top',
-                    '[data-pjax]',
-                    '.pjax-reload'
-                ]
-            });
-        } catch (e) {
-            console.warn('PJAX error: ' + e);
+const pjax = new Pjax({
+  selectors: [
+    'head title',
+    'meta[property="og:title"]',
+    'script[type="application/json"]',
+    // Precede .main-inner to prevent placeholder TOC changes asap
+    '.post-toc-wrap',
+    '.main-inner',
+    '.languages',
+    '.pjax'
+  ],
+  switches: {
+    '.post-toc-wrap': function(oldWrap, newWrap) {
+      if (newWrap.querySelector('.post-toc')) {
+        Pjax.switches.outerHTML.call(this, oldWrap, newWrap);
+      } else {
+        const curTOC = oldWrap.querySelector('.post-toc');
+        if (curTOC) {
+          curTOC.classList.add('placeholder-toc');
         }
+        this.onSwitch();
+      }
     }
+  },
+  analytics: false,
+  cacheBust: false,
+  scrollTo : !CONFIG.bookmark.enable
+});
 
-    // // Listen for start of Pjax
-    // document.addEventListener('pjax:send', function() {
-    //     return;
-    //     // TODO pace start loading animation
-    // })
-
-    // // Listen for completion of Pjax
-    // document.addEventListener('pjax:complete', function() {
-    //     return;
-    //     // TODO pace stop loading animation
-    // })
-
-    document.addEventListener('DOMContentLoaded', () => initPjax());
-}());
+document.addEventListener('pjax:success', () => {
+  pjax.executeScripts(document.querySelectorAll('script[data-pjax]'));
+  NexT.boot.refresh();
+  // Define Motion Sequence & Bootstrap Motion.
+  if (CONFIG.motion.enable) {
+    NexT.motion.integrator
+      .init()
+      .add(NexT.motion.middleWares.subMenu)
+      // Add sidebar-post-related transition.
+      .add(NexT.motion.middleWares.sidebar)
+      .add(NexT.motion.middleWares.postList)
+      .bootstrap();
+  }
+  if (CONFIG.sidebar.display !== 'remove') {
+    const hasTOC = document.querySelector('.post-toc:not(.placeholder-toc)');
+    document.querySelector('.sidebar-inner').classList.toggle('sidebar-nav-active', hasTOC);
+    NexT.utils.activateSidebarPanel(hasTOC ? 0 : 1);
+    NexT.utils.updateSidebarPosition();
+  }
+});
